92f4a795f5004b9f53e0f6ab50a9679c79d92810
novel-iwasaki <naoya.iwasaki@novel.jpn.com>
2020-03-01 21:55:00 +0900

    Base setting w/ lua

---
+++ lua/plugins.lua
@@ -0,0 +1,87 @@
+vim.cmd.packadd "packer.nvim"
+
+require("packer").startup(function()
+  use {
+    "morhetz/gruvbox",
+    config = function()
+      vim.cmd([[
+        colorscheme gruvbox
+        set t_Co=256
+        let &t_AB="\e[48;5;%dm"
+        let &t_AF="\e[38;5;%dm"
+        set background=dark
+      ]])
+    end
+  }
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+      opt = true
+    },
+    options = { theme = 'gruvbox' }
+  }
+
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    tag = 'nightly',
+    config = function()
+      require('config.nvim-tree')
+      vim.cmd([[
+        noremap <C-n> :NvimTreeToggle<CR>
+      ]])
+    end,
+  }
+
+  use "neovim/nvim-lspconfig"
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
+
+  use {
+    "hrsh7th/nvim-cmp",
+    requires = {
+      {'L3MON4D3/LuaSnip'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'hrsh7th/vim-vsnip'},
+    },
+    config = function()
+      require('config.snippets')
+      require('config.cmp')
+    end,
+  }
+
+  use {
+    "sheerun/vim-polyglot",
+    config = function()
+      vim.g.csv_no_conceal = 1
+    end
+  }
+
+  use "airblade/vim-gitgutter"
+  use "tpope/vim-rhubarb"
+  use "tpope/vim-fugitive"
+  use "junegunn/fzf"
+  use "junegunn/fzf.vim"
+
+  use {
+    "mattn/emmet-vim",
+    opt = true,
+    config = vim.cmd([[
+      imap <C-e> <Plug>(emmet-expand-abbr)
+    ]]),
+    ft = {"vue", "javascriptreact", "html", "sass", "scss", "css"}
+  }
+
+  use {
+    "udalov/kotlin-vim",
+    opt = true,
+    ft = {"kotlin"}
+  }
+end)
